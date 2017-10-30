99c1cb55d1bbe674e57b258fb73617ee95b1fac5
renchaochao <ren709271967@163.com>
2017-10-30 10:05:04 +0800

    vue-todolist

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+   <HelloWorld></HelloWorld>
+   </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+export default {
+  name: 'app',
+  data(){
+     return {
+	 }
+  },
+  components:{HelloWorld}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+ul{
+   width:400px;
+   margin:30px auto;
+   border:1px solid #ddd;
+   min-height:60px;
+}
+li{
+   border-bottom:1px dotted #dfdfdf;
+}
+li .del{color:red;margin-left:20px;cursor:pointer;}
+li.check{
+  text-decoration:line-through;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="vue-todolist" class="todolistDiv">
+    <span> todolist</span>
+        <input class="ipt" type="text" v-model="inputVal" v-on:keyup.enter="addItem"/>
+        <ul >
+            <li v-for="(item,index) in items" :key="index">
+            <div class="liDiv">
+                <input type="checkbox" v-model="item.isChecked" >
+                <label  v-bind:class="{ complete:item.isChecked }">{{item.text}}</label>
+                <span @click="delItem(index)" class="btn">X</span>
+            </div>
+           </li>
+        </ul>
+		<p>已选中{{activecounts}}/共{{items.length}}</p>
+    </div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+     return {
+	   items:[],
+       inputVal:""
+	 }
+  },
+  computed:{
+     activecounts(){
+	    var total = 0; 
+		this.items.forEach(function(item){ 
+		if(item.isChecked){
+		   total++;
+		} 
+		}); 
+		return total;
+	 }
+  },
+  methods:{
+      addItem(){
+        this.items.push({text:this.inputVal});
+		console.log(this.items);
+        this.inputVal="";
+    },
+    delItem(index) {
+      this.items.splice(index, 1);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+.complete{color:red;}
+</style>
